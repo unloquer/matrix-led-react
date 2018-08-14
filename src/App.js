ff22072781d344992e6e70d904dbd8b3aec63206
Alexander <johnnysepulveda988@gmail.com>
2018-08-14 17:21:47 -0500

    quito comentarios que estorban

--- src/App.js
+++ src/App.js
@@ -4,38 +4,15 @@ import React, { Component } from 'react';
 import Matrix from './components/Matrix'
 import './App.css';
 
-/*
-const colores = [ 
-  "blackButton",
-  "whiteButton",
-  "greenButton",
-  "blueButton",
-  "redButton" 
-];
 
-*/
 class App extends Component {
-/*
-  constructor(){
-    super();
-    this.state = { color: 0 }; 
-  }
 
-  changeColor = () => {
-    this.setState({ color: (this.state.color + 1) % colores.length })
-  }
-*/
   render(){
-    
-    /*let state = colores[this.state.color];*/
-
     return (
       <div className="App">
           <Matrix> </Matrix>
       </div>
     );
-
-    /*console.log(colores);*/
   }
 
 }
