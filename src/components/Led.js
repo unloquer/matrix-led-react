2e918ed1ee3ab234850b740a6ae14b33350f33ed
Alexander <johnnysepulveda988@gmail.com>
2018-08-14 20:12:30 -0500

    este componente queda pendiente de enviar su state al componente matrix

--- src/components/Led.js
+++ src/components/Led.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './componentes.css';
 
-const Led = () => (
-	<div className="envLed"> 
-		<p className="botones whiteButton"> Led </p> 
-	</div>
-);
+
+const colores = [ 
+  "blackButton",
+  "whiteButton",
+  "greenButton",
+  "blueButton",
+  "redButton" 
+];
+
+class Led extends Component { 
+
+    constructor(){
+    	super();
+    	this.state = { color: 0 }; 
+  	}
+
+  	changeColor = () => {
+    	this.setState({ color: (this.state.color + 1) % colores.length })
+  	}
+
+	render(){
+		let state = colores[this.state.color];
+		return (
+				<div className="envLed"> 
+					<p className={state} onClick={this.changeColor}> Led </p> 
+				</div>
+		);
+	}
+};
 
 export default Led;
