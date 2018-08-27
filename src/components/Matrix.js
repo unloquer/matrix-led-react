e2435ea38bc8f10494c674c1e477141464bd5e77
Alexander <johnnysepulveda988@gmail.com>
2018-08-26 20:52:05 -0500

    Inserto el componente Led usando un Object.keys para acceder a cada objeto del array y luego con .map añado un key a cada objeto

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Led from './Led';
 import './componentes.css';
 
 
-const Matrix = () => (
-	<div id="Matrix"> 
-		<Led></Led>
-	</div>
-);
+class Matrix extends Component{ 
+	
+	constructor(props){
+		super();
+	} 
+	
+	render(){
+		//console.log(this.props.leds)
+		const {color, f, c, nombre} = this.props.leds;
+
+		return (			
+
+			<div id="Matrix"> 
+				{/* Nos devuelve cada objeto de la metrix de objetos 
+				luego le asignamos un key con map
+				*/}
+				{Object.keys(this.props.leds).map(key => (
+					<Led
+						key={key}
+						nombre={nombre}
+						fila={f}
+						columna={c}
+						color={color}
+					/>
+				)) }
+			</div>
+		)
+	}
+}
 
 
 
