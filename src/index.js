db5530aa6b0cc9b7f8248010943e8b49482420d9
Alexander <johnnysepulveda988@gmail.com>
2018-08-26 20:53:21 -0500

    concto el componente aplicación el con root de public

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/Aplicacion.css';
+import Aplicacion from './components/Aplicacion';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Aplicacion />, document.getElementById('root'));
 registerServiceWorker();
