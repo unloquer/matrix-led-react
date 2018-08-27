918d1665d943a44532b2c5b99d0c570c4c3f04a0
Alexander <johnnysepulveda988@gmail.com>
2018-08-26 20:51:00 -0500

    necesito leer aqui las props que se inyectan por el object en el componente matrix

--- src/components/Led.js
+++ src/components/Led.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
+//import Leds from './Leds.js';
 import './componentes.css';
 
-const colores_ = [ 
-	{nombre: "led 1", f:2, c:1, color: "whiteButton"},
-	{nombre: "led 2", f:3, c:1, color: "greenButton"},
-	{nombre: "led 3", f:4, c:1, color: "blueButton"},
-	{nombre: "led 4", f:1, c:1, color: "blackButton"},
-	{nombre: "led 5", f:1, c:2, color: "greenButton"},
-	{nombre: "led 6", f:1, c:2, color: "blackButton"}, 
-	{nombre: "led 7", f:1, c:2, color: "whiteButton"}, 
-	{nombre: "led 8", f:1, c:2, color: "greenButton"},  
-  ];
-
 class Led extends Component { 
-
-    constructor(){
-    	super();
-    	this.state = { color: 0 }; 
+	constructor({bombre, fila, columna, color}){
+		super();
+		this.state = {
+			color : color
+		};
   	}
 
   	envioEstado = () => {
@@ -24,20 +15,30 @@ class Led extends Component {
   	}
 
   	changeColor = () => {
-    	this.setState({ color: (this.state.color + 1) % colores_.length });
-    	console.log("posicion: "+this.state.color);
+		  
+		this.setState({ color: (this.state.color + 1) % 9 });
+		console.log("posicion: "+this.state.color);
+		
   	}
   	
 	render = () => {
-		const estado = colores_[this.state.color];
-        const clases = `botones ${estado.color}`;
+		/*
+		const estado = COLOR[this.state.color];
+		*/
+		const clases = `botones ${this.state.color}`;
 
+		//const {color} = this.state;
+		//const {nombre} = this.props;
+		console.log()
 		return (
-				<div className="envLed"> 
-					<p className={clases} onClick={this.changeColor}> {this.name} </p> 
-				</div>
-		);
+			//let nombre = this.props.leds;	
+			<div className="envLed"> 
+				<p className={clases} onClick={this.changeColor}> </p> 
+			</div>
+				
+		)
 	}
+	
 };
 
 export default Led;
