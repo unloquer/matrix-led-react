a933c4bde39ef805d5a8e25243b6ee8653317809
Alexander <johnnysepulveda988@gmail.com>
2018-08-28 21:56:25 -0500

    componente que se la añade el evento click para el cambio de color

--- src/components/Led.js
+++ src/components/Led.js
@@ -1,39 +1,45 @@
 import React, { Component } from 'react';
-//import Leds from './Leds.js';
 import './componentes.css';
 
+const coloresLeds = [
+	"whiteButton",
+	"greenButton",
+	"blueButton",
+	"blackButton",
+	"greenButton",
+	"blackButton", 
+	"whiteButton", 
+	"greenButton"
+];
+
 class Led extends Component { 
-	constructor({bombre, fila, columna, color}){
+	
+	constructor(){
 		super();
 		this.state = {
-			color : color
-		};
-  	}
-
+			color: 0
+		}
+	  }
+	  
   	envioEstado = () => {
   		console.log("hola");
   	}
 
   	changeColor = () => {
-		  
-		this.setState({ color: (this.state.color + 1) % 9 });
-		console.log("posicion: "+this.state.color);
+		this.setState({ color: (this.state.color + 1) % coloresLeds.length });
+		//console.log("posicion: "+this.state.color);
 		
   	}
   	
-	render = () => {
-		/*
-		const estado = COLOR[this.state.color];
-		*/
-		const clases = `botones ${this.state.color}`;
+	render(){
+		//console.log(this.props);
+		const estado = coloresLeds[this.state.color];
+		const cambio = `botones ${estado}`;
 
-		//const {color} = this.state;
-		//const {nombre} = this.props;
-		console.log()
 		return (
 			//let nombre = this.props.leds;	
 			<div className="envLed"> 
-				<p className={clases} onClick={this.changeColor}> </p> 
+				<p className={cambio} onClick={this.changeColor}></p> 
 			</div>
 				
 		)
