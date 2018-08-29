2556b6553638f5663ef161543dd073c2677aa8e2
Alexander <johnnysepulveda988@gmail.com>
2018-08-28 21:57:35 -0500

    añado un export que contiene un array de objetos

---
+++ src/arrayLeds.js
@@ -0,0 +1,10 @@
+export const arrayLeds = [ 
+	{nombre: "led 1", f:2, c:1, color: "whiteButton"},
+	{nombre: "led 2", f:3, c:1, color: "greenButton"},
+	{nombre: "led 3", f:4, c:1, color: "blueButton"},
+	{nombre: "led 4", f:1, c:1, color: "blackButton"},
+	{nombre: "led 5", f:1, c:2, color: "greenButton"},
+	{nombre: "led 6", f:1, c:2, color: "blackButton"}, 
+	{nombre: "led 7", f:1, c:2, color: "whiteButton"}, 
+	{nombre: "led 8", f:1, c:2, color: "greenButton"},  
+  ];
