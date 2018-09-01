507eee0236699cab7a48da0ef9117a33659641aa
Alexander <johnnysepulveda988@gmail.com>
2018-09-01 08:39:57 -0500

    recibo el state del compoenente led en una prop

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import Led from './Led';
-import './componentes.css';
+import '../css/componentes.css';
 import {arrayLeds} from '../arrayLeds';
 
 class Matrix extends Component{ 
@@ -10,25 +10,30 @@ class Matrix extends Component{
 		this.arrayLeds = arrayLeds;
 	} 
 
-	enviarEstadoLed = estadoled => {
-		console.log("este es el estado y key de cada led:", estadoled);
+	reciboEstadoLed = estadoled => {
+		//console.log("este es el estado y key de cada led:", estadoled);
 		// TRABAJAR AQUI
+	}
+
+	pintoLeds = () => {
+		// Nos devuelve cada objeto de la metrix de objetos 
+		// luego le asignamos un key con map
+		return (
+			Object.keys(arrayLeds).map(key => (
+				<Led 
+					key={key}
+					keyid={key}
+					reciboEstadoLed={this.reciboEstadoLed}
+				/>
+			)) 
+		)
 	}
 	
 	render(){
-
-		return (			
+		return (	
+				
 			<div id="Matrix"> 
-				{/* Nos devuelve cada objeto de la metrix de objetos 
-				luego le asignamos un key con map
-				*/}				
-				{Object.keys(arrayLeds).map(key => (
-					<Led 
-						key={key}
-						keyid={key}
-						enviarEstadoLed={this.enviarEstadoLed}
-					/>
-				)) }
+				{this.pintoLeds()}
 			</div>
 		)
 	}
