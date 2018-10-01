59a2a251427bcecd96e449277b87c4e46e9df8e9
Alexander <johnnysepulveda988@gmail.com>
2018-09-30 21:00:00 -0500

    del componente matrix saco su state y se lo mando a la función socket, para enviar la state de la matrix por el socket

--- src/components/Aplicacion.js
+++ src/components/Aplicacion.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import '../css/componentes.css';
 import Matrix from './Matrix';
+import { enviarSocket } from '../socket';
 
 class Aplicacion extends Component {
-	/*
-	constructor(){
-		super();
-		this.state = {
-			leds : []
-		};
+
+	reciboState = (estadoLeds) => {
+		//console.log("en aplicacion");
+		//console.log(estadoLeds.ledsState);
+		enviarSocket(estadoLeds.ledsState);
 	}
-	*/
+
 	render = () => {
 		return (
-      <Matrix/>
+      		<Matrix reciboState={this.reciboState}/>
 		);
 	}
 };
