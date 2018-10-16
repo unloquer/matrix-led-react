3e8f60f7381df73108dd912bc1c60dd3e3a7202b
Alexander <johnnysepulveda988@gmail.com>
2018-10-16 13:47:08 -0500

    props para añadir alertas

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -16,10 +16,38 @@ class Matrix extends Component{
   reciboEstadoLed = (keyid,estadoled) => {
     this.setState({ ledsState: R.update(keyid, estadoled, this.state.ledsState) })
     this.props.reciboStateLeds( {ledsState: R.update(keyid, estadoled, this.state.ledsState)} );
+    
+  }
+
+  pintoAlertas = () => {
+    // aqui llegan las alertas
+    console.log(this.props.inyectoAlertas)
+    let figura = []
+
+    if(this.props.inyectoAlertas === null)
+    {
+      return null;
+    }
+    else if(this.props.inyectoAlertas === 'verde')
+    {
+      figura = [1,1,1,1,0,1,0,1,0,1,
+                1,1,1,1,0,1,0,1,0,1,
+                1,1,1,1,0,1,0,1,0,0,
+                1,1,1,1,0,1,0,1,0,0,
+                1,1,1,1,0,1,0,1,0,0,
+                1,1,1,1,0,1,0,1,0,0,
+                1,0,1
+              ]
+              
+      this.setState({
+        ledsState: figura
+      })
+      
+    }
+    
   }
 
   pintoLeds = () => {
-    console.log(this.props.inyectoAlertas)
     return (
       Object.keys(arrayLeds).map((key,idx) => (
         <Led
